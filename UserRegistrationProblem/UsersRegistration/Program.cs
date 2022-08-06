587a92f0508e6c3607b17511fb7b20bb546e4d2b
ravi04ez <ezekie.em@gmail.com>
2022-08-06 17:42:54 +0530

    Added Validation of Mobile Format

--- UserRegistrationProblem/UsersRegistration/Program.cs
+++ UserRegistrationProblem/UsersRegistration/Program.cs
@@ -12,10 +12,11 @@ do
     Console.WriteLine("1.Enter a Valid First Name Start with upper case & Min. 3 Characters ");
     Console.WriteLine("2.Enter a Valid Last Name Start with upper case & Min. 3 Characters ");
     Console.WriteLine("3.Enter a Valid Email Address E.g.- abc.xyz@bl.co.in");
+    Console.WriteLine("4.Enter a valid Mobile Format E.g.-91 9989218789  ");
     
     // Taking Input From User to Execute Question
     Console.Write("\nPlease Enter Question Number :- ");
-    cChoice=Convert.ToInt32(Console.ReadLine());
+    Choice=Convert.ToInt32(Console.ReadLine());
 
     switch (Choice)
     {
@@ -27,6 +28,9 @@ do
             break;
         case 3:
             Console.WriteLine(Registration.EmailAddress("Email Address"));
+            break;
+        case 4:
+            Registration.MobileFormat();
             break;
         default:
             Console.WriteLine("Please Enter Correct Question Number");
--- UserRegistrationProblem/UsersRegistration/Registration.cs
+++ UserRegistrationProblem/UsersRegistration/Registration.cs
@@ -9,9 +9,9 @@ namespace UsersRegistration
 {
     public class Registration
     {
+        // Method for Validation First & Last Name
         public static string Name(string name)
         {
-            //Regular Expression for Validation of First Name
             string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
 
             Console.Write("\nEnter {0} Name :- ", name);
@@ -20,12 +20,29 @@ namespace UsersRegistration
             //if Name Enter By User is match with Expression  then Valid if not Then Inva;id 
             return Regex.IsMatch(EnteredName, REGEX_FIRSTNAME) ? (name + "Name is Valid") : (name + " Name is Invalid");
         }
+        // Method for Validation of Email Address
         public static string EmailAddress(string Email)
         {
             string REGEX_Email = "^([A-Za-z]){3,}([_.+-]{1}[0-9A-Za-z]{1,}){0,}@[a-z0-9]+[.](com|net|co){0,1}([.]((com )|([a-z]{2}){0,1})){0,1}$";
             Console.Write("\nEnter {0} :- ",Email);
             string EnteredEmail=Console.ReadLine();
             return Regex.IsMatch(EnteredEmail, REGEX_Email) ? (Email + " is Valid") : (Email + " is Invalid");
+        }
+        // Method for Validation Of Mobile Format
+        public static void MobileFormat()
+        {
+            Console.Write("\nEnter Mobile number with country code: ");
+            var MobileNo = Console.ReadLine();
+            string PhoneNo = "^[91]{2}[\\s][6-9][0-9]{9}$";
+            Regex regex4 = new Regex(PhoneNo);
+            if (regex4.IsMatch(MobileNo))
+            {
+                Console.WriteLine("Mobile number is valid");
+            }
+            else
+            {
+                Console.WriteLine("Mobile number is Invalid!!");
+            }
         }
     }
 }
