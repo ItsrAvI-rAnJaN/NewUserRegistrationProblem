9a1a38a578de8bbb49bf3b84c0f3777a94cd68d8
ravi04ez <ezekie.em@gmail.com>
2022-08-06 06:04:50 +0530

    Added Validation for First Name

--- UserRegistrationProblem/UsersRegistration/Program.cs
+++ UserRegistrationProblem/UsersRegistration/Program.cs
@@ -4,4 +4,25 @@ using System;
 Console.WriteLine("\t\t\t\t######################################################");
 Console.WriteLine("\t\t\t\t\tWELCOME TO USERS REGISTRATION PROBLEM");
 Console.WriteLine("\t\t\t\t######################################################");
+int Choice;
+
+do
+{
+    Console.WriteLine("\n 0.Exit");
+    Console.WriteLine("1.Enter a Valid First Name Start with upper case & Min. 3 Characters ");
+    Console.Write("\nPlease Enter Question Number :- ");
+    Choice=Convert.ToInt32(Console.ReadLine());
+
+    switch (Choice)
+    {
+        case 1:
+            Console.WriteLine(Registration.Name("First"));
+            break;
+        default:
+            Console.WriteLine("Please Enter Correct Question Number");
+            break;
+    }
+
+}
+while (Choice != 0);
 Console.ReadKey();
--- UserRegistrationProblem/UsersRegistration/Registration.cs
+++ UserRegistrationProblem/UsersRegistration/Registration.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace UsersRegistration
 {
     public class Registration
     {
+        public static string Name(string name)
+        {
+            //Regular Expression for Validation of First Name
+            string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
 
+            Console.Write("\nEnter {0} Name :-  ", name);
+            string EnteredName = Console.ReadLine();
+
+            //if Name Enter By User is match with Expression  then Valid if not Then Inva;id 
+            return Regex.IsMatch(EnteredName, REGEX_FIRSTNAME) ? (name + "Name is Valid") : (name + " Name is Invalid");
+        }
     }
 }
