82219172c0ad27c8e0fc19938b20bb4356dde104
ravi04ez <ezekie.em@gmail.com>
2022-08-06 05:39:40 +0530

    Wlcm Msg UsersRegistration

---
+++ UserRegistrationProblem/UsersRegistration/Program.cs
@@ -0,0 +1,7 @@
+﻿using UsersRegistration;
+using System;
+
+Console.WriteLine("\t\t\t\t######################################################");
+Console.WriteLine("\t\t\t\t\tWELCOME TO USERS REGISTRATION PROBLEM");
+Console.WriteLine("\t\t\t\t######################################################");
+Console.ReadKey();
